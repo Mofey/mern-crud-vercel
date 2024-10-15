a07b5146447ef45af93f82c660ad07348f0ea92a
mofey <mofey@outlook.com>
2024-10-15 06:39:22 +0100

    vercel-day2

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 
 // Replace with your actual backend URL
 const BACKEND_URL = process.env.NODE_ENV === 'production'
-    ? 'https://your-backend-domain.vercel.app/api/products' // Replace with your actual backend URL
+    ? 'https://backend-kohl-delta.vercel.app/' // Replace with your actual backend URL
     : '/api/products';
 
 export const useProductStore = create((set) => ({
