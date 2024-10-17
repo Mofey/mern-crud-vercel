66050742e9130e8917220d6fe661f2e1a1e3dea1
mofey <mofey@outlook.com>
2024-10-17 12:29:25 +0100

    vercel-day4

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,11 @@ app.use(cors({
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
+// Add a simple route to check if the server is running
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
 // Product routes
 app.use("/api/products", productRoutes);
 
@@ -33,15 +38,9 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-
 app.listen(PORT, () => {
     connectDB();
     console.log("Server started at http://localhost:" + PORT);
-});
-
-// Add a simple route to check if the server is running
-app.get('/', (req, res) => {
-    res.send('Server is running');
 });
 
 export default app;
