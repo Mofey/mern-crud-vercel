791a5df7fcbaadefc51dad4fd1b97daa6e39797b
mofey <mofey@outlook.com>
2024-10-17 11:44:29 +0100

    vercel-day4

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 
 // Replace with your actual backend URL
 const BACKEND_URL = process.env.NODE_ENV === 'production'
-    ? 'https://mern-crud-vercel-jxez.vercel.app/' // Replace with your actual backend URL
+    ? 'https://mern-crud-vercel-api.vercel.app/' // Replace with your actual backend URL
     : '/api/products';
 
 export const useProductStore = create((set) => ({
